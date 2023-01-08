3d10bb2f631fbbfe4b5e0dd97ec0a9a9be38eb64
romanobro56 <romanp2929@gmail.com>
2023-01-08 16:58:25 -0500

    lell

---
+++ src/pages/Romanocraft.tsx
@@ -0,0 +1,11 @@
+import { Component } from 'solid-js';
+
+const Romanocraft: Component = () => {
+    return (
+        <div>
+            <h2>Romanocraft</h2>
+        </div>
+    )
+}
+
+export default Romanocraft;
